565972868f92e70e3a580d3312f75d2adb6b61ed
Josh Kenyon <joshuakenyon1@gmail.com>
2025-10-28 15:17:36 -0300

    checkout page

---
+++ src/modules/checkout/ui/components/checkout-item.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { cn, formatCurrency } from "@/lib/utils";
+import Image from "next/image";
+
+interface CheckoutItemProps {
+    isLast?: boolean;
+    imageUrl: string | null;
+    name: string;
+    productUrl: string;
+    tenantUrl: string;
+    tenantName: string;
+
+    price: number;
+    onRemove: () => void;
+}
+
+const CheckoutItem = ({
+    isLast,
+    imageUrl,
+    name,
+    productUrl,
+    tenantUrl,
+    tenantName,
+
+    price,
+    onRemove,
+}: CheckoutItemProps) => {
+  return (
+    <div
+        className={cn(
+            "grid grid-cols-[8.5rem_1fr_auto] gap-4 pr-4 border-b",
+            isLast && "border-0"
+        )}
+    >
+        <div className="overflow-hidden border-r">
+            <div className="relative aspect-square h-full">
+                <Image 
+                    src={imageUrl || "/placeholder-image.png"} 
+                    alt={name} 
+                    fill 
+                    className="object-cover"
+                />
+
+            </div>
+
+        </div>
+        <div className="py-4 flex flex-col justify-between">
+            <div>
+                <Link href={productUrl}>
+                    <h4 className="font-bold underline">{name}</h4>
+                </Link>
+                <Link href={tenantUrl}>
+                    <p className="font-medium underline">{tenantName}</p>
+                </Link>
+            </div>
+
+        </div>
+        <div className="py-4 flex flex-col justify-between">
+            <p className="font-medium">
+                {formatCurrency(price)}
+            </p>
+            <button 
+                onClick={onRemove} 
+                className="underline font-medium cursor-pointer">
+                Remover
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default CheckoutItem;
---
+++ src/modules/checkout/ui/components/checkout-sidebar.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import { CircleXIcon } from "lucide-react";
+import { format } from "path";
+
+interface CheckoutSidebarProps {
+  total: number;
+  isPending: boolean;
+  isCancelled: boolean;
+  onCheckout: () => void;
+}
+
+export const CheckoutSidebar = ({
+  total,
+  isPending,
+  isCancelled,
+  onCheckout,
+}: CheckoutSidebarProps) => {
+  return (
+    <div className="border rounded-md overflow-hidden bg-white flex flex-col">
+      <div className="flex items-center justify-between p-4 border-b">
+        <h4 className="font-medium text-lg">Total</h4>
+        <p className="font-medium text-lg">{formatCurrency(total)}</p>
+      </div>
+      <div className="p-4 flex items-center justify-center">
+        <Button
+          variant="elevated"
+          disabled={isPending}
+          onClick={onCheckout}
+          size="lg"
+          className="text-base w-full text-white bg-primary hover:bg-emerald-400 hover:text-primary"
+        >
+          Finalizar Compra
+        </Button>
+      </div>
+      {isCancelled && (
+        <div className="p-4 flex justify-center items-center border-t">
+          <div className="bg-red-100 border border-red-400 font-medium px-4 py-3 rounded flex items-center w-full">
+            <div className="flex items-center">
+              <CircleXIcon className="fill-red-600 mr-2 size-6 text-red-100" />
+              <span>Falha no checkout. Por favor, tente novamente.</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
