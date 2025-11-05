52b514adf84d89a3609b953efc8cafb7f978b2d8
Josh Kenyon <joshuakenyon1@gmail.com>
2025-11-05 12:05:20 -0300

    deployment fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(tenantSlug: string) {
-  if (process.env.NODE_ENV === "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_SUBDOMAIN_ROUTING!);
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/lojas/${tenantSlug}`;
   }
 
